c4101a6feb6c5b49b347393a5a903bde810060f9
Mark Goadrich <goadrich@hendrix.edu>
2021-01-29 10:26:23 -0600

    Added sprites, starting GUI

---
+++ PianoMoversDemo/Assets/Scripts/Annotations.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Annotations : MonoBehaviour
+{
+
+    // Background image from https://opengameart.org/content/city-silhouhette
+    // Building https://opengameart.org/content/corporate-building
+    // Piano https://pixabay.com/vectors/piano-grand-piano-baby-grand-piano-31357/
+    // Van https://commons.wikimedia.org/wiki/File:Box_truck_side_low_poly.png
+    // Net by Mark Goadrich
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
